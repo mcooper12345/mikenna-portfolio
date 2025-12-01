4bdb391c60c2de7a79c15586fe7668a39b38991e
Your Name <you@example.com>
2025-12-01 10:21:04 -0500

    Add hair page

---
+++ app/hair/page.js
@@ -0,0 +1,10 @@
+export default function HairPage(){
+    return(
+        <main className="p-8">
+        <h1 className="text-4x1 font-bold">Hair Booking</h1>
+        <p className="mt-4 text-lg text-gray-600">
+            My booking page will go here soon.
+            </p>
+        </main>
+    );
+}
